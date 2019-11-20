1676c5fac69cfb078d5cce138db425b4f1e6fc0c
Mauricio Homma <mhhomma@gmail.com>
2019-11-20 10:05:02 -0300

    initial commit

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,45 @@
+import mapboxgl from 'mapbox-gl';
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
+};
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb'
+    });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+      const element = document.createElement('div');
+        element.className = 'marker';
+        element.style.backgroundImage = `url('${marker.image_url}')`;
+        element.style.backgroundSize = 'contain';
+        element.style.width = '25px';
+        element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
+        .addTo(map);
+    });
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                          mapboxgl: mapboxgl }));
+
+    fitMapToMarkers(map, markers);
+  }
+};
+
+export { initMapbox };
